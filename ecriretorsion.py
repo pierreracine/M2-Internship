284d8f92f60552085b5a7e4126cb10473b5b1f3e
pierreracine <60776428+pierreracine@users.noreply.github.com>
2021-06-09 11:39:04 +0200

    Update ecriretorsion.py

--- ecriretorsion.py
+++ ecriretorsion.py
@@ -19,9 +19,9 @@ with open("tritorsion.txt","r") as toread, open ("torsion.txt","w") as tofill :
             continue
         if liste[4]=='4':
             tofill.write("torsion "+liste[0]+"  "+liste[1]+"  "+liste[2]+"  "+liste[3]+"  "+
-                         "0.0 0.0 1 "+liste[5]+" 180 2 0.0 0.0 3 \n")
+                         "0.0 0.0 1 "+liste[5]+"  90 2 0.0 0.0 3 \n")
             continue
         if liste[4]=='5':
             tofill.write("torsion "+liste[0]+"  "+liste[1]+"  "+liste[2]+"  "+liste[3]+"  "+
-                         liste[6]+" 180 1 "+liste[5]+" 180 2 0.0 0.0 3 \n")
+                         liste[6]+" 180 1 "+liste[5]+"  90 2 0.0 0.0 3 \n")
             continue
